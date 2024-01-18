959d9af208c8d758c2f197d8a029147ac27dd7b2
Inlavid <119132031+VasileiosKokki@users.noreply.github.com>
2024-01-18 22:44:19 +0200

    Add files via upload

--- teliko.c
+++ teliko.c
@@ -39,35 +39,58 @@ void on_sig_term(int sig) {
 
 // -------------------- Functions ------------------ //
 
-
-void parent_process(int pipe_fd, int done_pipe_fd, int i) {
+void parent_process1(int pipe_fd, int done_pipe_fd, int i) {
     char message[100];
     // Format the message with the child number
     sprintf(message, "Hello child, I am your father and I call you: 'Slave %d'", i);
     write(pipe_fd, message, strlen(message));
+}
 
 
-    // Read the message from the pipe
-    char done_msg[100];
-    ssize_t bytes_read = read(done_pipe_fd, done_msg, sizeof(done_msg));
-    if (bytes_read == -1) {
-        perror("read");
+void parent_process2(int pipe_fd, int done_pipe_fd, int i) {
+
+
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(done_pipe_fd, &read_fds);
+    
+    struct timeval timeout;
+    timeout.tv_sec = 5;  // Set your desired timeout in seconds
+    timeout.tv_usec = 0;
+
+    int select_result = select(done_pipe_fd + 1, &read_fds, NULL, NULL, &timeout);
+    if (select_result == -1) {
+        perror("select");
         exit(1);
+    } else if (select_result == 0) {
+        printf("Timeout occurred. No data available for reading.\n");
+        // Handle timeout logic if needed
+    } else {
+
+
+        char message[100];
+        // Read the message from the pipe
+        char done_msg[100];
+        ssize_t bytes_read = read(done_pipe_fd, done_msg, sizeof(done_msg));
+        if (bytes_read == -1) {
+            perror("read");
+            exit(1);
+        }
+        done_msg[bytes_read] = '\0';
+        printf("%s\n", done_msg);
+        // Find the position of "'" in the message
+        char *colon_position = strchr(done_msg, '\'');
+        char *message_after_colon;
+        if (colon_position != NULL) {
+            message_after_colon = colon_position + 1; // Skip "' " or "'"
+        }
+        char *quote_position = strchr(message_after_colon, '\'');
+        if (quote_position != NULL) {
+            *quote_position = '\0'; // Null-terminate the string at the single quote position
+        }
+        sprintf(message, "Done received from '%s'", message_after_colon);
+        write(pipe_fd, message, strlen(message));
     }
-    done_msg[bytes_read] = '\0';
-    printf("%s\n", done_msg);
-    // Find the position of "'" in the message
-    char *colon_position = strchr(done_msg, '\'');
-    char *message_after_colon;
-    if (colon_position != NULL) {
-        message_after_colon = colon_position + 1; // Skip "' " or "'"
-    }
-    char *quote_position = strchr(message_after_colon, '\'');
-    if (quote_position != NULL) {
-        *quote_position = '\0'; // Null-terminate the string at the single quote position
-    }
-    sprintf(message, "Done received from '%s'", message_after_colon);
-    write(pipe_fd, message, strlen(message));
 }
 
 
@@ -153,10 +176,6 @@ int main(int argc, char *argv[]) {
     int gonethroughere = 0;
 
 
-    const char *semName = "myfilelockFork1";  // thetei to onoma tou shmaforou iso me myfilelockFork
-    sem_t *sem_filelock;  // Dhlonei thn kleidaria
-    sem_filelock = sem_open(semName, O_CREAT, 0600, 2);  // Anoigoume ton shmaforo me onoma semName (myfilelockFork) kai thn parametro O_CREAT
-                                                         // pou ton dhmiourgei MONO an den iparxei , me dikaiomata gia ton xrhsth kai timh 1
     
     
     
@@ -197,27 +216,29 @@ int main(int argc, char *argv[]) {
             close(pipes[i][0]); // Close the read end of the communication pipe
             close(done_pipes[i][1]); // Close the write end of the communication2 pipe
             child_pids[i] = child_pid;
-            sem_wait(sem_filelock);
-            parent_process(pipes[i][1], done_pipes[i][0], i);
-            sem_post(sem_filelock);
-            close(pipes[i][1]); // Close the read end of the communication pipe
-            close(done_pipes[i][0]); // Close the write end of the communication2 pipe
+            parent_process1(pipes[i][1], done_pipes[i][0], i);
+
+            
             
         } else {
             
             // This is the child process
             close(pipes[i][1]); // Close the write end of the communication pipe
             close(done_pipes[i][0]); // Close the read end of the communication2 pipe
-            sem_wait(sem_filelock);
             child_process(pipes[i][0], done_pipes[i][1], file_fd);
-            sem_post(sem_filelock);
-            close(pipes[i][0]); // Close the write end of the communication pipe
-            close(done_pipes[i][1]); // Close the read end of the communication2 pipe
+            close(pipes[i][0]); // Close the read end of the communication pipe
+            close(done_pipes[i][1]); // Close the write end of the communication2 pipe
 
             signal(SIGTERM,(void (*)(int))on_sig_term);
             sleep(20);
             exit(0);
         }
+    }
+
+    for (int i = 0; i < child_count; i++) {
+        parent_process2(pipes[i][1], done_pipes[i][0], i);
+        close(pipes[i][1]); // Close the read end of the communication2 pipe
+        close(done_pipes[i][0]); // Close the read end of the communication2 pipe
     }
     
 
@@ -229,8 +250,6 @@ int main(int argc, char *argv[]) {
     printf("Children have been gracefully shut down\n");
     close(file_fd); // Close the file descriptor after the child_process 
     free(child_pids); // Free the dynamically allocated array when done
-    sem_close(sem_filelock);  // Kleinei ton shmaforo
-    sem_unlink(semName);  // Aposindeei to onoma tou shmaforou apo ton shmaforo
     return 0;
 
     
